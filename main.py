7d5e026f6f0e5bfe03b80e04169ced4d97a0f9b5
rajahaider007 <janjuaalihaider786@gmail.com>
2025-05-06 22:36:42 +0500

    Initial commit or updated files

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices') 
-engine.setProperty('voice', voices[0].id)
-engine.setProperty('rate', 150) 
+import speech_recognition as sr
 
-def speak(command):
-    engine.say(command)
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 150)
+
+
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
-speak("How are you ?")
+
+
+# speak("How are you ?")
+
+
+def commmand():
+    content = " "
+    while content == " ":
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+        try:
+            content = r.recognize_google(audio, language="en-in")
+            print("You Said........... " + content)
+        except Exception as e:
+            print("Please try again... ")
+
+        return content
+
+
+while True:
+    request = commmand()
+    print(request)
